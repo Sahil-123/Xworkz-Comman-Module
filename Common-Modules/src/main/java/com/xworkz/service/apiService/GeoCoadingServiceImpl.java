788bcf56384f27db2791d1b9083de84acb8e4c35
Sahil-123 <sahilnaikwadi123@gmail.com>
2024-09-12 04:02:13 +0530

    js geocoading.js file changed for JSP too.

--- Common-Modules/src/main/java/com/xworkz/service/apiService/GeoCoadingServiceImpl.java
+++ Common-Modules/src/main/java/com/xworkz/service/apiService/GeoCoadingServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -29,69 +30,99 @@ public class GeoCoadingServiceImpl implements GeoCoadingService {
     @Cacheable(cacheNames = "shortLivedCache", key = "'countries'")
     @Override
     public List<CountryDTO> getCountries() throws URISyntaxException {
+        try {
+            System.out.println("Fetching Countries from server");
+            String url = "https://www.universal-tutorial.com/api/countries";
 
-        System.out.println("Fetching Countries from server");
-        String url = "https://www.universal-tutorial.com/api/countries";
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("User-Agent", "Application");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("User-Agent", "Application");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
+            HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
+            ResponseEntity<List<CountryDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
+                    new ParameterizedTypeReference<List<CountryDTO>>() {
+                    });
 
-        ResponseEntity<List<CountryDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
-                new ParameterizedTypeReference<List<CountryDTO>>() {
-                });
+            System.out.println(responseEntity.getBody());
+            return responseEntity.getBody();
 
-        System.out.println(responseEntity.getBody());
-        return responseEntity.getBody();
+        } catch (
+                HttpClientErrorException e) {
+            System.err.println("Error occurred during API call: " + e.getStatusCode());
+            System.err.println("Response body: " + e.getResponseBodyAsString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Collections.emptyList();
     }
 
     @Cacheable(cacheNames = "shortLivedCache", key = "#country")
     @Override
     public List<StateDTO> getStates(String country) throws URISyntaxException {
-        System.out.println("Fetching States from server");
+        try {
+            System.out.println("Fetching States from server");
 
-        String url = "https://www.universal-tutorial.com/api/states/" + country;
+            String url = "https://www.universal-tutorial.com/api/states/" + country;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("User-Agent", "Application");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("User-Agent", "Application");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
 
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
+            HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-        ResponseEntity<List<StateDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
-                new ParameterizedTypeReference<List<StateDTO>>() {
-                });
+            ResponseEntity<List<StateDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
+                    new ParameterizedTypeReference<List<StateDTO>>() {
+                    });
 
-        System.out.println(responseEntity.getBody());
-        return responseEntity.getBody();
+            System.out.println(responseEntity.getBody());
+            return responseEntity.getBody();
+        } catch (
+                HttpClientErrorException e) {
+            System.err.println("Error occurred during API call: " + e.getStatusCode());
+            System.err.println("Response body: " + e.getResponseBodyAsString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Collections.emptyList();
     }
 
     @Cacheable(cacheNames = "shortLivedCache", key = "#state")
     @Override
     public List<CityDTO> getCities(String state) throws URISyntaxException {
-        System.out.println("Fetching Cities from server");
+        try {
+            System.out.println("Fetching Cities from server");
 
-        String url = "https://www.universal-tutorial.com/api/cities/" + state;
+            String url = "https://www.universal-tutorial.com/api/cities/" + state;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("User-Agent", "Application");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("User-Agent", "Application");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.set("Authorization", "Bearer " + geoCoding.getAccessTocken());
 
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
+            HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-        ResponseEntity<List<CityDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
-                new ParameterizedTypeReference<List<CityDTO>>() {
-                });
+            ResponseEntity<List<CityDTO>> responseEntity = restTemplate.exchange(new URI(url), HttpMethod.GET, entity,
+                    new ParameterizedTypeReference<List<CityDTO>>() {
+                    });
 
-        System.out.println(responseEntity.getBody());
-        return responseEntity.getBody();
+            System.out.println(responseEntity.getBody());
+            return responseEntity.getBody();
+        } catch (
+                HttpClientErrorException e) {
+            System.err.println("Error occurred during API call: " + e.getStatusCode());
+            System.err.println("Response body: " + e.getResponseBodyAsString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Collections.emptyList();
     }
 }
